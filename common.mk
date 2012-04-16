382be87caa3db288e869248eb34c253991c0167e
Clifford Hung <chung@rim.com>
2012-04-16 14:19:30 -0400

    Commit of what's in the SVN repo
    http://foundry51.qnx.com/svn/repos/internal-source/trunk/QREncoder

---
+++ common.mk
@@ -0,0 +1,33 @@
+# This is an automatically generated record.
+# The area between QNX Internal Start and QNX Internal End is controlled by
+# the QNX IDE properties.
+
+ifndef QCONFIG
+QCONFIG=qconfig.mk
+endif
+include $(QCONFIG)
+
+#===== USEFILE - the file containing the usage message for the application. 
+USEFILE=
+
+#===== EXTRA_SILENT_VARIANTS - variants that are not appended to the result binary name (like MyBin_g)
+EXTRA_SILENT_VARIANTS+=v7
+
+include $(MKFILES_ROOT)/qmacros.mk
+#===== INSTALL_DIRECTORY - Base directory where builder installs result binary files.
+INSTALLDIR=lib
+
+define PINFO
+PINFO name=QREncoder
+endef
+
+ifndef QNX_INTERNAL
+QNX_INTERNAL=$(PROJECT_ROOT)/.qnx_internal.mk
+endif
+include $(QNX_INTERNAL)
+
+include $(MKFILES_ROOT)/qtargets.mk
+
+OPTIMIZE_TYPE_g=none
+OPTIMIZE_TYPE=$(OPTIMIZE_TYPE_$(filter g, $(VARIANTS)))
+
---
+++ config.h
@@ -0,0 +1,13 @@
+/*
+ * config.h
+ *
+ *  Created on: Jun 7, 2010
+ *      Author: HLITTLE
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define __STATIC static
+
+#endif /* CONFIG_H_ */
